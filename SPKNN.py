9f92823631d40401fda3e1ee9ee6ea2780ae7c44
CDenis21 <99687690+CDenis21@users.noreply.github.com>
2022-02-25 08:51:33 -0500

    Add files via upload

--- SPKNN.py
+++ SPKNN.py
@@ -1,11 +1,41 @@
+from re import X
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import sklearn 
 
-from sklearn import KNN
-from sklearn.model_selection import train_test_split
-
-raw_data = pd.read_csv('.csv')
+raw_data = pd.read_csv('datasetf.csv')
 print(raw_data)
+
+def convertToFloat(row):
+    X = raw_data[['Destination','Length','Qname','QType','DNS_TTL']].values
+    
+    newArray = []
+    for i, r in enumerate(row):
+            if i == 0:
+                newArray.append(r)
+                continue
+            if i == len(row) - 1:
+                newArray.append(r)
+                break
+            newArray.append(float(r))
+    return newArray
+
+def convertToFloat(column):
+    y = raw_data[['Source']].values
+    newArray = []
+    for i, r in enumerate(column):
+            if i == 0:
+                newArray.append(r)
+                continue
+            if i == len(column) - 1:
+                newArray.append(r)
+                break
+            newArray.append(float(r))
+    return newArray
+
+
+from sklearn.neighbors import KNeighborsClassifier
+neigh = KNeighborsClassifier(n_neighbors=3)
+neigh.fit(X, y)

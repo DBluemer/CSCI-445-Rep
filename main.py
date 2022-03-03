29162ac4174fe1c4b069cedd6bbe3bebbe2ae4a9
abdulahad12345 <abdulahadghuman441@gmail.com>
2022-03-03 13:41:54 -0500

    Connect Python to Database

---
+++ main.py
@@ -0,0 +1,7 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()

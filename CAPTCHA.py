5459f1ddd1a10af73dcbdc0a8fb99c3206f9cf5c
DBluemer <93562283+DBluemer@users.noreply.github.com>
2022-03-08 13:35:22 -0500

    Update CAPTCHA.py

--- CAPTCHA.py
+++ CAPTCHA.py
@@ -1,24 +1,19 @@
+import random
+from captcha.image import ImageCaptcha
 class CAPTCHA:
     def generateCaptcha(self):
-        # Import the following modules
-    from captcha.image import ImageCaptcha
 
-    #import random module to generate character string
+        result_str = ''.join(
+            (random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789') for i in range(5)))
 
+        # Create an image instance of the given size
+        image = ImageCaptcha(width=280, height=90)
 
-#create random string param (test 5)
+        # Image captcha text will equal random result
+        captcha_text = result_str
 
-    result_str = ''.join(
-        (random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789') for i in range(5)))
+        # generate the image of the given text
+        data = image.generate(captcha_text)
 
-    # Create an image instance of the given size
-    image = ImageCaptcha(width=280, height=90)
-
-    # Image captcha text will equal random result
-    captcha_text = result_str
-
-    # generate the image of the given text
-    data = image.generate(captcha_text)
-
-    # write the image on the given file and save it
-    image.write(captcha_text, 'CAPTCHAman.png')
+        # write the image on the given file and save it
+        image.write(captcha_text, 'CAPTCHAman.png')

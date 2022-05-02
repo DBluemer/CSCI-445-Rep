7e8c990aefc5ef93ab140b1f4cd1cdb73ba2ea0c
DBluemer <93562283+DBluemer@users.noreply.github.com>
2022-05-02 17:06:52 -0400

    Add files via upload
    Can be run using 'python implement.py', currently saying on my machine that "'python' is not a known command'

---
+++ implement.py
@@ -0,0 +1,19 @@
+import base64
+import json
+import requests
+
+url = "https://csciteam4bds.com/"
+user = "csciteam"
+password = "C5c!T3amF0ur"
+credentials = user + ':' + password
+token = base64.b64encode(credentials.encode())
+header = {'Authorization': 'Basic ' + token.decode('utf-8')}
+post = {
+ 'title'    : 'test123',
+ 'status'   : 'publish',
+ 'content'  : 'This is my first post created using rest API',
+ 'categories': 5,
+ 'date'   : '2020-01-05T10:00:00'
+     }
+response = requests.post(url, headers=header, json=post)
+print(response)

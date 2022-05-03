229f3a05eac01bca3a16a8c3333bae6955f0772f
DBluemer <93562283+DBluemer@users.noreply.github.com>
2022-05-03 18:36:26 -0400

    Add files via upload

--- post.py
+++ post.py
@@ -10,29 +10,12 @@ import os
 ########################### Read Me First ###############################
 '''
 ------------------------------------------In DETAIL--------------------------------
-Description
-===========
-Add new posts to WordPress remotely using Python using XMLRPC library provided by the WordPress.
-Installation Requirement
-************************
-Verify you meet the following requirements
-==========================================
-Install Python 2.7 (Don't download 3+, as most libraries dont yet support version 3). 
-Install from PyPI using easy_install python-wordpress-xmlrpc 
-Easy_Install Link: https://pypi.python.org/pypi/setuptools
-==========================================
-Windows Installation Guide
-==========================
--Download and Install Easy_Install from above Link -Extract Downloaded File and from CMD go to the extracted directory and run 'python setup.py install'. This will install easy_install. -Go to %/python27/script and run following command easy_install python-wordpress-xmlrpc
-Ubuntu Installation Guide
-=========================
-sudo apt-get install python-setuptools 
-sudo easy_install python-wordpress-xmlrpc
-Note: Script has its dummy data to work initially which you can change or integrate with your code easily for making it more dynamic.
-****************************************
-For Bugs/Suggestions
-contact@waqasjamal.com
-****************************************
+This code is being borrowed from @waqasjamal, but is being updated to run in python 3, as the libraries used are 
+no longer in use / updated.
+
+To run properly, you must first 'pip install python-wordpress-xmlrpc' in your terminal
+
+
 ------------------------------------------In DETAIL--------------------------------		
 '''
 
